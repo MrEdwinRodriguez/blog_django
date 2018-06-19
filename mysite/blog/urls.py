aa4c160f8d67e64eec5cc0df576fb2a0c001a101
edwinrodriguez <mredwinrodriguez@gmail.com>
2018-06-18 21:36:18 -0400

    working blog

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 from blog import views
 
 urlpatterns = [
-    url(r'^$', views.PostListViewView.as_view(), name='post_list'),
+    url(r'^$', views.PostListView.as_view(), name='post_list'),
     url(r'^about/$', views.AboutView.as_view(), name='about'),
-    url(r'^post/', views.PostDetailView.as_view(), name='post_detail'),
+    url(r'^post/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post_detail'),
     url(r'^post/new/$', views.CreatePostView.as_view(), name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.PostUpdateView.as_view(), name='post_edit'),
     url(r'^post/(?P<pk>\d+)/remove/$', views.PostDetailView.as_view(), name='post_remove'),
